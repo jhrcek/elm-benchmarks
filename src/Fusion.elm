c55c712acb6ddfe53ec8087da8ef6a7ca79f469b
Jan Hrček <honza.hrk@gmail.com>
2019-05-07 06:30:20 +0200

    Compare map fusion

---
+++ src/Fusion.elm
@@ -0,0 +1,29 @@
+module ListConcat exposing (main)
+
+import Benchmark exposing (Benchmark, benchmark, describe)
+import Benchmark.Runner exposing (BenchmarkProgram, program)
+
+
+main : BenchmarkProgram
+main =
+    program suite
+
+
+suite : Benchmark
+suite =
+    describe "map fusion" <|
+        [ benchmark "map (f . g)" <| \() -> List.map (f << g) xs
+        , benchmark "map f . map g" <| \() -> List.map f <| List.map g xs
+        ]
+
+
+f x =
+    x + 1
+
+
+g x =
+    x * 2
+
+
+xs =
+    List.range 1 1000
